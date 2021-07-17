e3180992b229ddc7f9a4755a064f1c577d76c39c
shoshori <k.a39katsu1@gmail.com>
2021-07-18 01:06:42 +0900

    2021/07/18

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,4 +6,27 @@ class PostsController < ApplicationController
   def show
     @post = Post.find_by(id: params[:id])
   end
+
+  def new
+  end
+
+  def edit
+    @post= Post.find_by(id: params[:id])
+  end
+
+  def index
+    @posts = Post.all.order(created_at: :desc)
+  end
+
+  def create
+    @post = Post.new(content: params[:content])
+    @post.save
+    redirect_to("/posts/index")
+  end
+
+  def update
+    @post = Post.find_by(id: params[;id])
+    @post.content=params[:cotent]
+    @post.save
+    redirect_to("posts/index")
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
-  get 'posts/index'
+  get 'posts/index'=>"posts#index"
+  get "posts/new"=>"posts#new"
+  post "posts/create"=> "posts#create"
+  get  "posts/:id/edit"=>"posts#edit" 
+  post "posts/;id/update"=>"posts#update"
   get "/"=>"home#top"
   get "about"=>"home#about"
   get "posts/:id" =>"posts#show"
+  
   
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
